679e24d0e2248e3371b1ffdc4d7e2e688e6b0aa6
unknown <anishkumar92@gmail.com>
2020-07-05 00:04:59 +0530

    menu fix

--- JoyBookings/src/app/places/offers/offer-bookings/offer-bookings-routing.module.ts
+++ JoyBookings/src/app/places/offers/offer-bookings/offer-bookings-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { OfferBookingsPage } from './offer-bookings.page';
+import { OfferBookingsPage } from "./offer-bookings.page";
 
 const routes: Routes = [
   {
-    path: '',
-    component: OfferBookingsPage
-  }
+    path: "",
+    component: OfferBookingsPage,
+  },
 ];
 
 @NgModule({
