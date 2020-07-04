6f390ae9e03ccc29f9951cc39da45f06ebdb94b1
unknown <anishkumar92@gmail.com>
2020-07-04 20:40:29 +0530

    router and navigation

--- JoyBookings/src/app/places/discover/place-detail/place-detail.page.ts
+++ JoyBookings/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { ActivatedRoute } from "@angular/router";
+
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
 
 @Component({
-  selector: 'app-place-detail',
-  templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  selector: "app-place-detail",
+  templateUrl: "./place-detail.page.html",
+  styleUrls: ["./place-detail.page.scss"],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("placeId")) {
+        this.navCtrl.navigateBack("/places/discover");
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
+    });
   }
 
+  onBookPlace() {
+    this.navCtrl.navigateBack("/places/discover");
+  }
 }
