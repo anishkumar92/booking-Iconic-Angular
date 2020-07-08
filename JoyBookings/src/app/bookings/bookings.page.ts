0f34c595959e8df5771fdc9ea4ba318e4b55e5f7
unknown <anishkumar92@gmail.com>
2020-07-08 23:50:18 +0530

    cancel booking

--- JoyBookings/src/app/bookings/bookings.page.ts
+++ JoyBookings/src/app/bookings/bookings.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BookingService } from './booking.service';
 import { Booking } from './booking.model';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,7 +13,10 @@ export class BookingsPage implements OnInit, OnDestroy {
   private bookingSub: Subscription;
   loadedBookings: Booking[];
 
-  constructor(private bookingService: BookingService) {}
+  constructor(
+    private bookingService: BookingService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
     this.bookingSub = this.bookingService.bookings.subscribe((bookings) => {
@@ -21,9 +24,14 @@ export class BookingsPage implements OnInit, OnDestroy {
     });
   }
 
-  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+  onCancelBooking(bookingId: string, slidingEl: IonItemSliding) {
     slidingEl.close();
-    // cancel booking wiht id offerId
+    this.loadingCtrl.create({ message: 'Cancelling...' }).then((loadingEl) => {
+      loadingEl.present();
+      this.bookingService.cancelBooking(bookingId).subscribe(() => {
+        loadingEl.dismiss();
+      });
+    });
   }
 
   ngOnDestroy() {
